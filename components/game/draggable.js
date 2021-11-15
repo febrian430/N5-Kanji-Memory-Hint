5a55f930621f8169156441c433ae41a60a92574e
Febrian <febrian430@gmail.com>
2021-11-16 01:03:24 +0700

    add pick and drop

---
+++ components/game/draggable.js
@@ -0,0 +1,119 @@
+import React, { useRef, useState, useEffect } from "react";
+import { Animated, PanResponder, StyleSheet, Text, View } from "react-native";
+
+// https://stackoverflow.com/questions/56738500/react-native-onlayout-with-react-hooks
+//TODO: 
+// cleaner way to get abs pos of every options
+// add selected state
+// set selected when animated object is selected (get by index? maybe selected index?)
+// maybe make options as normal view and spawn an animated view on press
+// when component dropped to area, judge by selected state
+
+const CORRECT = 1
+const WRONG = 0
+const NOT_IN_AREA = -1
+
+const Draggable = ({value, onDrop}) => {
+
+  const [val, setVal] = useState(null)
+  const [coordinate, setCoordinate] = useState({x: 0, y: 0})
+  const [solved, setSolved] = useState(false)
+
+  const pan = useRef(new Animated.ValueXY()).current
+  const opacity = useRef(new Animated.Value(1)).current
+  
+  useEffect(() => {
+    setVal(value)
+  }, [])
+
+  const panStyle = {
+    transform: pan.getTranslateTransform()
+  }
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+    onMoveShouldSetPanResponder: (evt, gestureState) => true,
+    onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+
+    onPanResponderGrant: (evt, gestureState) => {
+        // The gesture has started. Show visual feedback so the user knows
+        // what is happening!
+        // gestureState.d{x,y} will be set to zero now
+        // console.log("ON TOUCH", gestureState)
+        console.log(gestureState)
+        // pan.setOffset({x: coordinate.x-40, y: coordinate.y-40})
+      },
+
+
+    onPanResponderMove: Animated.event([
+      null, { dx: pan.x, dy: pan.y }], {useNativeDriver: false}),
+
+    onPanResponderRelease: (evt, gestureState) => {
+      console.log("ON RELEASE", gestureState)
+
+      const value = onDrop(val,
+        {
+          x: gestureState.moveX, 
+          y: gestureState.moveY
+        })
+      if(!solved) {
+        if(value === CORRECT) {
+          alert("correct")
+          Animated.timing(opacity, {
+            toValue: 0,
+            duration: 1000,
+            useNativeDriver: false
+          }).start(() => setSolved(true))
+        } else {
+          if(value === WRONG) {
+            alert("wrong")
+          }
+          Animated.spring(
+            pan, // Auto-multiplexed
+            { toValue: { 
+                x: 0, 
+                y: 0
+            }, useNativeDriver: false } // Back to zero
+          ).start();
+        }
+      }
+    },
+
+    onPanResponderTerminate: (evt, gestureState) => {
+        // Animated.spring(
+        //     pan, // Auto-multiplexed
+        //     { toValue: { 
+        //         x: pan.x, 
+        //         y: pan.y 
+        //     }, useNativeDriver: false } // Back to zero
+        //   ).start();
+    },
+  });
+
+  return (
+      <Animated.View
+        {...panResponder.panHandlers}
+        style={[pan.getLayout(), styles.box, {
+          opacity: opacity
+        }]}
+      >
+        <Text>{value}</Text>
+      </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  box: {
+    backgroundColor: "#61dafb",
+    width: 80,
+    height: 80,
+    borderRadius: 4,
+    borderWidth: 1
+  },
+});
+
+export default Draggable;
---
+++ components/game/picky.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Draggable from './draggable'
+
+const Picky = () => {
+
+    const onDropCorrect = (value, coordinate) => {
+        if(coordinate.y < 200) {
+            return 1
+        } else {
+            return -1
+        }
+    }
+
+    const onDropFalse = (value, coordinate) => {
+        if(coordinate.y < 200) {
+            return 0
+        } else {
+            return -1
+        }
+    }
+
+    return (
+        <View style={[styles.mainContainer, 
+        {
+            backgroundColor: "green"
+        }]}
+        >
+            <View style={styles.dropZone}>
+                <Text style={styles.text}>Drop them here!</Text>
+            </View>
+            <View style={styles.ballContainer} />
+            <View style={styles.row}>
+                <Draggable value="asd" onDrop={onDropCorrect} />
+                <Draggable value="def" onDrop={onDropFalse} />
+                <Draggable value="ghi" onDrop={onDropCorrect} />
+                <Draggable value="jkl" onDrop={onDropFalse} />
+                <Draggable value="mno" onDrop={onDropCorrect} />
+            </View>
+      </View>
+    )
+}
+
+export default Picky
+
+const styles = StyleSheet.create({
+    mainContainer: {
+      flex: 1
+    },
+    ballContainer: {
+      height:200
+    },
+    row: {
+      flexDirection: "row"
+    },  
+    dropZone: {
+      height: 100,
+      backgroundColor: "#00334d"
+    },
+    text: {
+      marginTop: 25,
+      marginLeft: 5,
+      marginRight: 5,
+      textAlign: "center",
+      color: "#fff",
+      fontSize: 25,
+      fontWeight: "bold"
+    }
+  });
