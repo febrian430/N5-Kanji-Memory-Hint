0366379192f4ecd8348b2801f7335ddb2ce7e345
Febrian <febrian430@gmail.com>
2021-11-20 01:18:57 +0700

    implement modes

--- components/game/jumble.js
+++ components/game/jumble.js
@@ -3,6 +3,7 @@ import { FlatList, ScrollView, Text, Touchable, View } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import Screen from '../cmps/screen'
 import { questionForJumble, optionsForJumble } from '../../helper/repo'
+import { FIELD, MODE } from '../const'
 
 const SENTINEL_VALUE = "-1"
 
@@ -10,6 +11,7 @@ const Slot = ({key, value, onPress}) => {
     var bgColor = "grey"
     var isSentinel = true
     if (value !== SENTINEL_VALUE){
+        console.log(value)
         bgColor = "pink"
         isSentinel = false
     }
@@ -38,13 +40,26 @@ const Jumble = ({navigation, route}) => {
     const [counter, setCounter] = useState(0)
     const [answer, setAnswer] = useState([])
 
+
+    const mode = route.params.mode
+
     useEffect(() => {
-        const QNA = questionForJumble()
+        function getKeys(mode) {
+            if(mode === MODE.IMAGE_MEANING) {
+              return [FIELD.IMAGE, FIELD.RUNE]
+            } else {
+              return [FIELD.RUNE, FIELD.SPELLING]
+            }
+        }
+
+        const [key1, key2] = getKeys(mode)
+
+        const QNA = questionForJumble(key1, key2)
         setQuestion(QNA.question)
         setOptions(QNA.options)
         const answer = QNA.question.key.split('')
         setAnswer(answer)
-        setSelected(new Array(answer.length).fill(SENTINEL_VALUE))
+        setSelected(new Array(answer.length).fill({id: -1, value: SENTINEL_VALUE}))
     }, [])
 
     useEffect(() => {
@@ -56,13 +71,13 @@ const Jumble = ({navigation, route}) => {
     }, [counter])
 
     const firstAssignableIndex = () => {
-        return selected.findIndex((value) => value === SENTINEL_VALUE)
+        return selected.findIndex((item) => item.value === SENTINEL_VALUE)
     }
 
     const unselect = (...indexes) => {
         let unselect = selected
         indexes.forEach((index) => {
-            unselect[index] = SENTINEL_VALUE
+            unselect[index] = {id: SENTINEL_VALUE, value: SENTINEL_VALUE}
         })
         setSelected([...unselect])
         setCounter(counter - indexes.length)
@@ -82,7 +97,7 @@ const Jumble = ({navigation, route}) => {
     const diff = (selects = [], target = []) => {
         let wrongIndexes = []
         target.forEach((value, index) => {
-            if(value !== selects[index]) {
+            if(value !== selects[index].value) {
                 wrongIndexes.push(index)
             }
         })
@@ -92,7 +107,7 @@ const Jumble = ({navigation, route}) => {
     const renderSelected = ({item, index}) => {
         return ( 
             <Slot 
-                value={item} 
+                value={item.value} 
                 onPress={() => slot.onPress(item, index)}
             />
         )
@@ -101,7 +116,7 @@ const Jumble = ({navigation, route}) => {
     const renderOptions = ({item, index}) => {
         return (
             <Option 
-                value={item}
+                value={item.value}
                 onPress={() => option.onPress(item, index)} 
                 disabled={selected.includes(item)}
             />
@@ -140,7 +155,7 @@ const Jumble = ({navigation, route}) => {
                 <FlatList 
                     data={selected}
                     renderItem={renderSelected}
-                    keyExtractor={(item,index) => item+index}
+                    keyExtractor={(item,index) => `${index}`}
                     extraData={selected}
                 />
             </View>
@@ -150,7 +165,7 @@ const Jumble = ({navigation, route}) => {
                     <FlatList 
                         data={options}
                         renderItem={renderOptions}
-                        keyExtractor={(item) => item}
+                        keyExtractor={(item) => item.id}
                     />
                 </ScrollView>
         </Screen>
