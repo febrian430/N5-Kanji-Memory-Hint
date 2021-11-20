a4c0f2435426a3ce1afd7af88a34bf9d8153b566
Febrian <febrian430@gmail.com>
2021-11-21 00:59:05 +0700

    update repo

--- helper/repo.js
+++ helper/repo.js
@@ -1,33 +1,17 @@
-import Distinct from '../assets/data/test.json'
+// import Distinct from '../assets/data/test.json'
 import Kanji from '../assets/data/kanji.json'
-// import Distinct from '../assets/data/distinct.json'
-
-
-
+import Distinct from '../assets/data/distinct.json'
 import shuffle from './shuffler';
-
-
-
-// const flatten = () => {
-//     let seen = new Map();
-//     let Kanji = []
-//     kanjis.kanjis.forEach(({chapter, examples}) => {
-//         examples.forEach(kanji => {
-//             if(seen.get(kanji.rune) === undefined) {
-//                 Kanji.push({ ...kanji, chapter: chapter })
-//                 seen.set(kanji.rune, kanji)
-//             }
-//         })
-//     })
-//     const json = JSON.stringify(Kanji)
-//     fs.writeFileSync("Kanji.json", json, "utf8")
-//     return Kanji
-// }
 
 
 const kanjisByChapter = (chapters) => {
     const kanjis = Kanji
     const filtered = kanjis.filter(kanji => chapters.includes(kanji.chapter))
+    return filtered
+}
+
+const questionsByChapters = (chapters) => {
+    const filtered = Distinct.filter(kanji => chapters.includes(kanji.chapter))
     return filtered
 }
 
@@ -43,28 +27,79 @@ const optionsForQuiz = (kanji, key) => {
 }
 
 //TODO: change to real kanji
-const questionForJumble = () => {
+const questionForJumble = (key1, key2) => {
     const selectedKanji = shuffle(Distinct)[0]
-    const question = { value: selectedKanji.rune, key: selectedKanji.spelling }
-    const options = optionsForJumble(selectedKanji)
+    const question = { value: selectedKanji[key1], key: selectedKanji[key2] }
+    const options = optionsForJumble(selectedKanji, key1, key2)
     return { question, options }
 }
 
 
 //TODO: change to real kanji
-const optionsForJumble = (kanji) => {
-    const NUM_OF_OPTIONS = 10
-    const spelling = kanji.spelling.split('')
-    const otherOpts = shuffle(Distinct.filter(({rune}) => rune !== kanji.rune)
-            .map(({spelling}) => spelling)
+const optionsForJumble = (question, questionKey, answerKey) => {
+    const NUM_OF_OPTIONS = 9
+    console.log(question)
+    const correctOption = question.key.split('')
+    const otherOpts = shuffle(Distinct.filter((example) => example[questionKey] !== question.value)
+            .map((example) => example[answerKey])
             .reduce((prev, val) => prev+val)
             .split('')
-    ).slice(0, NUM_OF_OPTIONS-spelling.length)
+    ).slice(0, NUM_OF_OPTIONS-correctOption.length)
 
-    const combined = [...spelling, ...otherOpts]
-    
+    let combined = [...correctOption, ...otherOpts]
+    combined = combined.map((value, index) =>  { return { value: value, id: String(index)}})
     return shuffle(combined)
 }
 
-export { Kanji, Distinct, kanjisByChapter, optionsForQuiz, questionsForQuiz, questionForJumble, optionsForJumble }
+const questionsForPicky = (chapters) => {
+    return shuffle(Distinct.filter(kanji => chapters.includes(kanji.chapter)))
+}
+
+const optionsForPicky = (kanji, key) => {
+    const otherOptions = shuffle(Distinct).filter((element) => element[key] !== kanji[key]).map((element) => element[key]).slice(0, 8)
+    let options = shuffle([kanji[key], ...otherOptions])
+
+    return options
+}
+
+const Jumble = {
+    getQuestion: (questionKey, answerKey, numOfQuestions = 1, selectedChapters = []) => {
+        
+        const kanjis = shuffle(questionsByChapters(selectedChapters)).slice(0, numOfQuestions)
+        const questions = kanjis.map((kanji) => { 
+            return { value: kanji[questionKey], key: kanji[answerKey], type: questionKey }
+        })
+        console.log(questions)
+        const qnas = questions.map((question) => {        
+            const opts = optionsForJumble(question, questionKey, answerKey)
+            return { question, options: opts }
+        })
+        console.log(qnas)
+        return qnas
+    }
+}
+
+const MixMatch = {
+    getQuestion: (questionKey, answerKey, numOfQuestions = 1, selectedChapters = []) => {
+        const kanjis = shuffle(questionsByChapters(selectedChapters)).slice(0, numOfQuestions)
+        const questions = kanjis.map((element) =>  { return { value: element[questionKey], key: element[answerKey] }})
+        const answers = kanjis.map((element) =>  { return { value: element[answerKey], key: element[questionKey] }})
+
+        return shuffle([...questions, ...answers])
+    }
+}
+
+export { 
+    Kanji, 
+    Distinct, 
+    kanjisByChapter, 
+    optionsForQuiz, 
+    questionsForQuiz, 
+    questionForJumble, 
+    optionsForJumble, 
+    questionsForPicky, 
+    optionsForPicky,
+    Jumble,
+    MixMatch
+}
 
